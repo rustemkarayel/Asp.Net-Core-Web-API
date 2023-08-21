9fc5ceb5a737f294aa84819298fe06e2f0075575
rustemkarayel <rustemkarayel07@gmail.com>
2023-08-21 21:54:11 +0300

    Add project files.

---
+++ WebApi/Program.cs
@@ -0,0 +1,44 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using WebApi.Repository;
+
+namespace WebApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            //dbContext'i kullanabilmek için. appsettings'deki veriyi configure etmek gerekir.Direkt elle buraya yazmak yerine appsettings kullanırız.
+            builder.Services.AddDbContext<RepositoryContext>(options=>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("sqlConnection")));
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
---
+++ WebApi/Repository/RepositoryContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi.Models;
+
+namespace WebApi.Repository
+{
+    public class RepositoryContext:DbContext
+    {
+        public RepositoryContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Book> Books { get; set; }
+    }
+}

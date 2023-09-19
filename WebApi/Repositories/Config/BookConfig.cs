36dde98736cf9260fa9a1a52f1e27acdd58c2d1f
rustemkarayel <rustemkarayel07@gmail.com>
2023-09-19 14:08:05 +0300

    Veriler yapılandırıldı.

---
+++ WebApi/Repositories/Config/BookConfig.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WebApi.Models;
+
+namespace WebApi.Repositories.Config
+{
+    public class BookConfig : IEntityTypeConfiguration<Book>
+    {
+        //CONFIG = tip konfigürasyonu = bazı alanlara default değerler vermek(çekirdek(seed) datalar girmek için) ve bazı alanları kısıtlamak için.
+        public void Configure(EntityTypeBuilder<Book> builder)
+        {
+            builder.HasData(
+                 new Book { Id = 1, Title = "Karagöz ve Hacivat", Price = 75 },
+                 new Book { Id = 2, Title = "Mesnevi", Price = 175 },
+                 new Book { Id = 3, Title = "Devlet", Price = 375 }
+                );
+
+        }
+    }
+}
---
+++ WebApi/Repositories/RepositoryContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi.Models;
+using WebApi.Repositories.Config;
+
+namespace WebApi.Repository
+{
+    public class RepositoryContext:DbContext
+    {
+        public RepositoryContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Book> Books { get; set; }
+
+        //CONFIG = tip konfigürasyonu = bazı alanlara default değerler vermek(çekirdek(seed) datalar girmek için) ve bazı alanları kısıtlamak için.
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new BookConfig());
+        }
+    }
+}
